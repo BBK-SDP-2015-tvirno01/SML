7f732f5a978338315e9c75fd0952b790db66aaaf
BBK-SDP-2015-tvirno01 <toby.virno@gmail.com>
2015-01-24 19:15:58 +0000

    Added instructions to Translator
    Added further case clauses for the other SML instructions to the switch
    statement

--- Translator.java
+++ Translator.java
@@ -90,8 +90,30 @@ public class Translator {
 			return new AddInstruction(label, r, s1, s2);
 		case "lin":
 			r = scanInt();
+			x = scanInt();
+			return new LinInstruction(label, r, x);
+		case "sub":
+			r = scanInt();
 			s1 = scanInt();
-			return new LinInstruction(label, r, s1);
+			s2 = scanInt();
+			return new SubInstruction(label, r, s1, s2);
+		case "mul":
+			r = scanInt();
+			s1 = scanInt();
+			s2 = scanInt();
+			return new MulInstruction(label, r, s1, s2);
+		case "div":
+			r = scanInt();
+			s1 = scanInt();
+			s2 = scanInt();
+			return new DivInstruction(label, r, s1, s2);
+		case "out":
+			s1 = scanInt();
+			return new OutInstruction(label, s1);
+		case "bnz":
+			s1 = scanInt();
+			s2 = scanInt();
+			return new BnzInstruction(label, s1, s2);
 		}
 
 		// You will have to write code here for the other instructions.
